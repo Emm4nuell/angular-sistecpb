551399de3a27e514c846d41c9c57ea439b90bff0
Emm4nuell <eduardo.eesf@gmail.om>
2023-09-21 22:53:07 -0300

    Criado a tela de CADASTRAR USUARIO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
+import { UsuarioCreateComponent } from './components/usuario/usuario-create/usuario-create.component';
+import { LoginComponent } from './components/login/login.component';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
+import {ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
-import { UsuarioCreateComponent } from './components/usuario/usuario-create/usuario-create.component';
+
 
 @NgModule({
   declarations: [
--- src/app/components/usuario/usuario-create/usuario-create.component.ts
+++ src/app/components/usuario/usuario-create/usuario-create.component.ts
@@ -8,8 +8,42 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class UsuarioCreateComponent implements OnInit {
 
+  nome = new FormControl('', [Validators.required]);
+  cpf = new FormControl('', [Validators.required])
   email = new FormControl('', [Validators.required, Validators.email]);
-  hide = true;
+  datanascimento = new FormControl('', [Validators.required]);
+  senha = new FormControl('', [Validators.required])
+  repitasenha = new FormControl('', [Validators.required])
+  btsenha = true;
+  btrepitasenha = true
+
+  mostrarsenha(): boolean{
+    if(this.btsenha == true){
+      this.btsenha = false
+      return this.btsenha;
+    }else{
+      this.btsenha = true;
+      return this.btsenha;
+    }
+  }
+
+  mostrarrepitasenha(): boolean{
+    if(this.btrepitasenha == true){
+      this.btrepitasenha = false
+      return this.btrepitasenha;
+    }else{
+      this.btrepitasenha = true;
+      return this.btrepitasenha;
+    }
+  }
+
+  validarCampo(): boolean{
+    if(this.nome.valid && this.cpf.valid && this.datanascimento.valid){
+      return true;
+    }else{
+      return false;
+    }
+  }
 
   constructor() { }
 
